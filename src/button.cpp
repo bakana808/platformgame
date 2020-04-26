08c3364ff6a0159ecd833d08ad8f80d3aca1ab84
Branden Akana <branden.akana@gmail.com>
2020-04-25 15:39:38 -1000

    move source and headers into correct folders and apply formatting

--- src/button.cpp
+++ src/button.cpp
@@ -1,29 +1,47 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include "button.h"
 using namespace std;
 
+//Start game option//
+Button::Button()
+{
+    tex.loadFromFile("menu.png");
+    menu.setTexture(tex);
+    menu.setScale(10, 10);
+    menu.setPosition(300, 300);
 
-
-
-
-//Button Constructor, Creating the button//
-Button::Button(int posx, int posy){
-    text.loadFromFile("startgamepic.png");
-    buttonImage.setTexture(text);
-    buttonImage.setColor(sf::Color(30,30,30,30);
-    buttonImage.setScale(.5,.5);
-    buttonImage.setPosition(200,200);
+    //Setings display Option//
+    tex.loadFromFile("settings.png");
+    settings.setTexture(tex);
+    settings.setScale(10, 10);
+    settings.setPosition(305, 305);
 }
 
-    //Checking if the Button was clicked, starting game//
-void Button::isButtonClicked( int posx, int posy){
-    if(button.image.getGlobalBounds().contains(posx,posy))
+//Menu button being clicked//
+void Button::isButtonClicked(int posx, int posy)
+{
+    if (button.image.getGlobalBounds().contains(300, 300))
+    {
         ButtonClicked();
-}
+
+        //How to play setting//
+        //Just prototype will make update soon//
+    if(button.image.getGlobalBounds().contains(305,305){
+            cout << " How to Play" << endl;
+            cout << " Press X to go back to main menu" << endl;
+            cout << "Directionals: " << endl;
+            cout << " Keyboard use" << endl;
+            cout << "WATCH OUT FOR ENEMIES AND OBSTACLES!" << endl;
+            cout << "HAVE FUN!" << endl;
+            //Draw new window//
+            ////Draw Text in window//
+            //if user presses x button, return to main menu//
+            //How to play instructions//
+
+	}
 
 
 
-void Button::ButtonClicked(){
+	void Button:: ButtonClicked(){
 
-}
+	}
