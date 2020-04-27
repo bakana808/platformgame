e4147488152793ab5f40431d9aea3df98bf9a6bf
Branden Akana <branden.akana@gmail.com>
2020-04-27 02:46:18 -1000

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
 
-#include <SFML/Graphics.hpp>
-// #include "player.h"
+#include <cmath>
+#include <vector>
+
+#include "common.h"
+#include "frame_clock.h"
+#include "player.h"
+#include "platform.h"
+#include "level.h"
 
 int main(void) {
 
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "Game");
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Game", 7U, settings);
 
-    // Player player;
+    FrameClock fc;
+
+    sf::View view({0.f, 0.f}, {1280.f, 720.f});
+    sf::View hud({0.f, 0.f}, {1280.f, 720.f});
+
+    Level level("level.txt");
+    Player player(view, hud);
+
+    player.set_level(level);
+
+    window.setFramerateLimit(200);
+    window.setKeyRepeatEnabled(false);
 
     while(window.isOpen()) {
+
+
+        float dt = fc.get_delta();
+
+        // PRINT(dt);
 
         sf::Event e;
 
@@ -16,24 +40,35 @@ int main(void) {
 
             if(e.type == sf::Event::EventType::KeyPressed) {
 
-                // player.key_press(e.key.code);
+                player.key_press(e.key.code);
                 if(e.key.code == sf::Keyboard::Escape) {
 
                     window.close();
                 }
             }
 
-
             if(e.type == sf::Event::EventType::KeyReleased) {
 
-                // player.key_release(e.key.code);
+                player.key_release(e.key.code);
             }
         }
 
-        window.clear();
+        // std::cout << delta << std::endl;
+        player.update(dt);
 
-        // window.draw(player);
+        window.clear({20, 20, 50});
+
+        window.setView(view);
+
+        window.draw(player);
+        window.draw(level);
+
+        window.setView(hud);
+
+        player.draw_hud(window);
 
         window.display();
+
+        fc.push_frame();
     }
 }
