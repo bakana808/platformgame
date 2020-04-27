69a161e7d92bae3f6dae16ae42b0fae8b4a14813
Branden Akana <branden.akana@gmail.com>
2020-04-27 02:50:37 -1000

    format health.h

--- include/health.h
+++ include/health.h
@@ -1,35 +1,33 @@
 #include <SFML/Graphics.hpp>
 
-			//Health.hpp//
-	class Health{
-	
-		public:
-			int playerhealth;
-			int enemyhealth;
-			int lives;
-			int death;
-			int hit_count;
-		
-		//Getting and setting player health//
-		//3 hit and he dies//
+//Health.hpp//
+class Health
+{
 
-		getplayerhealth const {return playerhealth};
-		sethealth(playerhealth=3);
-		
+public:
+    int playerhealth;
+    int enemyhealth;
+    int lives;
+    int death;
+    int hit_count;
 
-		//One hit he gone//
-		//Getting and setting enemy health//
-		getenemyhealth const {return enemyhealth};
-		setenemyhealth(enemyhealth = 1);
-		
-		//Health constructor//
-	Health();
+    //Getting and setting player health//
+    //3 hit and he dies//
 
-	//FUnction determininng if the player is dead//
-	void is_dead( int playerhealth, int hitcount);
-	
-	//Function displaying the number of lives//
-	void lives( int lives);
-		
+    int getplayerhealth() const { return playerhealth; }
+    void sethealth(int playerhealth = 3);
 
-	};
+    //One hit he gone//
+    //Getting and setting enemy health//
+    int getenemyhealth() const { return enemyhealth; }
+    void setenemyhealth(int enemyhealth = 1);
+
+    //Health constructor//
+    Health();
+
+    //FUnction determininng if the player is dead//
+    void is_dead(int playerhealth, int hitcount);
+
+    //Function displaying the number of lives//
+    void lives(int lives);
+};
