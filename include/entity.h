b54a8e5bb36e4c3e02be3b2fe4c03c21de4ddfbb
Branden Akana <branden.akana@gmail.com>
2020-05-01 15:22:00 -1000

    clean up entity.h

--- include/entity.h
+++ include/entity.h
@@ -1,6 +1,8 @@
 
 #pragma once
 
+#include <algorithm>
+#include <memory>
 #include "common.h"
 #include "vector/vec2.h"
 
@@ -23,44 +25,19 @@ public:
      * @brief Sets the position of this entity.
      */
     virtual void set_pos(const vec2&) = 0;
+
+
+    /**
+     * @brief Process a time step for this entity.
+     *
+     * @param delta the change in time from the last frame
+     */
+    virtual void update(float delta) = 0;
 };
 
 
-/**
- * @brief An object that is drawable and has a transform.
- * Serves as a wrapper for sf::Shape, sf::Sprite, and sf::Text objects.
- *
- */
-template<class DT>
-class GameObject: public Entity {
-public:
-
-    sf::Drawable * drawable;
-    sf::Transformable * transformable;
-
-    // DT => short for Drawable-Transformable
-    template<typename... Args>
-    GameObject(DT& parent) {
-
-        // compile-time asserts to ensure that the
-        // parent object inherits from these types.
-        static_assert(std::is_base_of<sf::Drawable, DT>::value, "Given object not a drawable");
-        static_assert(std::is_base_of<sf::Transformable, DT>::value, "Given object not a transformable");
-
-        drawable = (sf::Drawable*)(&parent);
-        transformable = (sf::Transformable*)(&parent);
-    }
-
-    void draw(sf::RenderTarget& target, sf::RenderStates states) const override {
-        target.draw(*drawable, states);
-    }
-
-    void set_pos(const vec2& pos) override {
-        transformable->setPosition(pos);
-    }
-
-    const vec2& get_pos(void) { return transformable->getPosition(); }
-};
+template<typename DT>
+class GameObject;
 
 
 /**
@@ -100,10 +77,10 @@ public:
      *
      * @param child the child to add
      */
-    void add_child(Entity& child) {
+    void add_child(Entity* child) {
 
-        this->bound_children.push_back(&child);
-        child.set_pos(pos);
+        this->bound_children.push_back(child);
+        child->set_pos(pos);
     }
 
     /**
@@ -111,27 +88,56 @@ public:
      *
      * @param child the child to add
      */
-    void add_child_free(Entity& child) {
+    void add_child_free(Entity* child) {
 
-        this->free_children.push_back(&child);
+        this->free_children.push_back(child);
     }
 
 
-    template<class DT, typename... Args>
+    /**
+     * @brief Removes and deletes a child entity.
+     * This will probably crash the program if the child isn't
+     * in allocated memory.
+     *
+     * @param child
+     */
+    void remove_child(Entity* child) {
+
+        PRINT("removing child " << child);
+        {
+            std::vector<Entity*>& vec = this->bound_children;
+            vec.erase(std::remove(vec.begin(), vec.end(), child), vec.end());
+        }
+        {
+            std::vector<Entity*>& vec = this->free_children;
+            vec.erase(std::remove(vec.begin(), vec.end(), child), vec.end());
+        }
+    }
+
+
+    /**
+     * @brief Add an object that inherits from both
+     * sf::Drawable and sf::Transformable.
+     *
+     * Should produce a compiler error if DT is not a sf::Drawable and
+     * sf::Transformable.
+     *
+     * @tparam DT
+     * @param obj
+     */
+    template<class DT>
     void add_child(DT& obj) {
 
-        GameObject<DT> *ent = new GameObject<DT>(obj);
-        PRINT("created wrapper for drawable-transformable");
-        this->add_child(*(Entity*)ent);
-        PRINT("wrapper for drawable-transformable");
+        GameObject<DT> *ent = new GameObject<DT>(std::unique_ptr<DT>(&obj), this);
+        this->add_child((Entity*)ent);
     }
+
 
     template<class DT>
     void add_child_free(DT& obj) {
 
-        GameObject<DT> *ent = new GameObject<DT>(obj);
-        PRINT("created wrapper for drawable-transformable");
-        this->add_child_free(*(Entity*)ent);
+        GameObject<DT> *ent = new GameObject<DT>(std::unique_ptr<DT>(&obj), this);
+        this->add_child_free((Entity*)ent);
     }
 
 
@@ -161,6 +167,7 @@ public:
 
     /**
      * @brief Set the position to a vector.
+     * This will also set the position of all child entities.
      *
      * @param pos
      */
@@ -173,6 +180,11 @@ public:
     }
 
 
+    /**
+     * @brief Get the position of this entity.
+     *
+     * @return const vec2&
+     */
     const vec2& get_pos(void) {
 
         return this->pos;
@@ -192,4 +204,87 @@ public:
             child->set_pos(pos);
     }
 
+
+    virtual void update(float delta) override {
+
+        // update all child entities
+
+        for(Entity* child: bound_children)
+            child->update(delta);
+
+        for(Entity* child: free_children)
+            child->update(delta);
+    }
 };
+
+
+/**
+ * @brief An object that is drawable and has a transform.
+ *
+ * Serves as a wrapper for any object that inherits both
+ * sf::Drawable and sf::Transformable.
+ * (e.g. sf::Shape, sf::Sprite, and sf::Text)
+ *
+ * This wrapper class was created because SFML lacks an interface that
+ * inherits both sf::Drawable and sf::Transformable.
+ *
+ * By using static_assert and std::is_base_of, the template class DT can be
+ * narrowed down to only those that inherit sf::Drawable and sf::Transformable,
+ * and should raise a compiler error if it doesn't.
+ *
+ * The term DT is short for Drawable-Transformable.
+ */
+template<class DT>
+class GameObject: public Entity {
+private:
+
+    CompositeEntity* source;
+
+    sf::Drawable * drawable;
+    sf::Transformable * transformable;
+
+public:
+
+    std::unique_ptr<DT> handle;
+
+    GameObject(std::unique_ptr<DT>&& handle, CompositeEntity* source = nullptr)
+    : source(source)
+    , handle(std::move(handle))
+    , drawable((sf::Drawable*)handle.get())
+    , transformable((sf::Transformable*)handle.get())
+    {
+        // compile-time asserts to ensure that the
+        // parent object inherits from these types.
+        static_assert(std::is_base_of<sf::Drawable, DT>::value, "Given object not a drawable");
+        static_assert(std::is_base_of<sf::Transformable, DT>::value, "Given object not a transformable");
+    }
+
+    ~GameObject() {
+
+        destroy();
+    }
+
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override {
+        target.draw(*drawable, states);
+    }
+
+    void set_pos(const vec2& pos) override {
+        transformable->setPosition(pos);
+    }
+
+    const vec2& get_pos(void) { return transformable->getPosition(); }
+
+
+    void update(float delta) override {}
+
+    /**
+     * @brief Delete this object. (self-delete)
+     *
+     */
+    void destroy() {
+
+        if(source)
+            source->remove_child(this);
+    }
+};
+
